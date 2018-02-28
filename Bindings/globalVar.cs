148fe3d399e60b4095e0a5bd5b0ec4a54e79ddf9
Felix Breuer <f.breuer94@gmail.com>
2018-02-28 19:09:03 +0100

    final code

---
+++ Bindings/globalVar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindings
+{
+    public static class globalVar
+    {
+        public const int SERVER_PORT = 5555;
+        public const int SERVER_MAX_PENDING_CONNECTIONS = 10;
+
+        public const int BUFFER_BYTE = 1024;
+        public const int DATA_SIZE_INFO_SIZE = 4;
+        public const int MAX_CLIENTS = 1000;
+    }
+}
---
+++ ClientApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Bindings;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ClientApp started.");
+
+            clientTCP client = new clientTCP();
+            client.setupClient();
+            client.connect();
+
+            client.handleFunctions.Add("string", handleStringMessage);
+
+            dataPackage pack = new dataPackage();
+            pack.write("string");
+            pack.write("hallo das ist meine message - CLIENT");
+
+            client.sendData(pack.toArray());
+            pack.Dispose();
+
+            Console.ReadLine();
+
+        }
+
+        public static void handleStringMessage(byte[] data)
+        {
+            dataPackage pack = new dataPackage();
+
+            pack.write(data);
+
+            Console.WriteLine(pack.readString());
+            Console.WriteLine(pack.readString());
+        }
+    }
+
+}
